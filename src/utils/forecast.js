82112bdf620b87791ddfbe55e87926f5f6d928d6
mpragnarok <mp922352612@gmail.com>
2019-08-21 22:24:00 +0800

    Add forecast string to include humidity and wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const request = require('request')
 
+
 const forecast = (lantitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/a6c6d7c62e22dd6584063c327eb061cb/${lantitude},${longitude}?units=si`
 
@@ -9,7 +10,10 @@ const forecast = (lantitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary}. It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary}. It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.
+            Wind speed is ${body.currently.windSpeed} m/s.
+            Humidity is ${body.currently.humidity} %
+            `)
         }
     })
 }
